4c7ce01ac6fdac120473391b5bdbcb61d4377aba
Jacob Kader (Currently not in use) <84777963+jacobkader@users.noreply.github.com>
2023-04-20 01:41:38 +0100

    updated a few components

--- navigator/Navigator.js
+++ navigator/Navigator.js
@@ -1,16 +1,17 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MenuProvider } from 'react-native-popup-menu';
-import React from 'react';
-import { useAuth } from './AuthContext';
-import ChatStackScreen from './ChatStack';
-import List from '../views/authorised/contacts/List';
-import UsersTabScreen from './UserStab';
-import Settings from '../views/authorised/account/SettingsT';
-import Settings2 from '../views/authorised/account/SettingsT2';
-import Home from '../views/unauthorised/startpage';
-import Login from '../views/unauthorised/LoginTest';
-import Register from '../views/unauthorised/RegisterTest';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MenuProvider } from "react-native-popup-menu";
+import React from "react";
+import { useAuth } from "./AuthContext";
+import ChatStackScreen from "./ChatStack";
+import List from "../views/authorised/contacts/List";
+import UsersTabScreen from "./UserStab";
+import Settings from "../views/authorised/account/SettingsT";
+import Settings2 from "../views/authorised/account/SettingsT2";
+import Home from "../views/unauthorised/startpage";
+import Login from "../views/unauthorised/LoginTest";
+import Register from "../views/unauthorised/RegisterTest";
+
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
